6c1d6c401e64da0961e497a43a3d31a1b96d735b
unnamedKnight <backfrontdev47@gmail.com>
2023-07-20 20:20:19 +0600

    Add password reset functionality

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -32,4 +33,37 @@ urlpatterns = [
     # account delete and profile management
     path("profile-management", views.profile_management, name="profile_management"),
     path("delete-account", views.delete_account, name="delete_account"),
+    # Password management urls/views
+    # 1 ) Submit our email form
+    path(
+        "reset_password",
+        auth_views.PasswordResetView.as_view(
+            template_name="account/password/password-reset.html"
+        ),
+        name="reset_password",
+    ),
+    # 2) Success message stating that a password reset email was sent
+    path(
+        "reset_password_sent",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="account/password/password-reset-sent.html"
+        ),
+        name="password_reset_done",
+    ),
+    # 3) Password reset link
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="account/password/password-reset-form.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    # 4) Success message stating that our password was reset
+    path(
+        "reset_password_complete",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="account/password/password-reset-complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
