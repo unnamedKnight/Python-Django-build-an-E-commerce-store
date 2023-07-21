b355a25ed9be75cc94c439b255c8b04f3b988098
unnamedKnight <backfrontdev47@gmail.com>
2023-07-21 11:48:50 +0600

    Add checkout functionality

--- src/payment/urls.py
+++ src/payment/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("checkout", views.checkout, name="checkout"),
     path("payment-success", views.payment_success, name="payment_success"),
     path("payment-failed", views.payment_failed, name="payment_failed"),
 ]
--- src/payment/views.py
+++ src/payment/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import ShippingAddress
 
 # Create your views here.
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        try:
+            shipping = ShippingAddress.objects.get(user=request.user)
+            context = {"shipping": shipping}
+            return render(request, "payment/checkout.html", context)
+        except ShippingAddress.DoesNotExist:
+            return render(request, "payment/checkout.html")
+    return render(request, "payment/checkout.html")
 
 
 def payment_success(request):
