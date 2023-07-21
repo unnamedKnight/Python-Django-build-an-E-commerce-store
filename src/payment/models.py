0514deac8e1c5833cbaa24c9ae49456bc267273f
unnamedKnight <backfrontdev47@gmail.com>
2023-07-21 19:28:59 +0600

    Add more models in payment app

--- src/payment/models.py
+++ src/payment/models.py
@@ -22,3 +22,28 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f"Shipping Address - {self.id}"
+
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=10000)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"Order - # {str(self.id)}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, null=True, blank=True
+    )
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"Order Item - # {str(self.id)}"
