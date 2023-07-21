c27d1dc3680d091ec7309881822069b175fbf430
unnamedKnight <backfrontdev47@gmail.com>
2023-07-21 20:34:41 +0600

    Complete checkout functionality

--- src/payment/urls.py
+++ src/payment/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("checkout", views.checkout, name="checkout"),
+    path("complete-order", views.complete_order, name="complete_order"),
     path("payment-success", views.payment_success, name="payment_success"),
     path("payment-failed", views.payment_failed, name="payment_failed"),
 ]
--- src/payment/views.py
+++ src/payment/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import ShippingAddress
+from django.http import JsonResponse
+from cart.cart import Cart
+
+from .models import ShippingAddress, Order, OrderItem
 
 # Create your views here.
 
@@ -15,7 +18,71 @@ def checkout(request):
     return render(request, "payment/checkout.html")
 
 
+def complete_order(request):
+    if request.POST.get("action") == "post":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        address1 = request.POST.get("address1")
+        address2 = request.POST.get("address2")
+        city = request.POST.get("city")
+        state = request.POST.get("state")
+        zipcode = request.POST.get("zipcode")
+
+        # all in one shipping address
+
+        shipping_address = (
+            address1 + "\n" + address2 + "\n" + city + "\n" + state + "\n" + zipcode
+        )
+        cart = Cart(request)
+        # get the total price of the items in the cart
+        total_cost = cart.get_total()
+
+        if request.user.is_authenticated:
+            order = Order.objects.create(
+                full_name=name,
+                email=email,
+                shipping_address=shipping_address,
+                amount_paid=total_cost,
+                user=request.user,
+            )
+
+            # order_id = order.pk
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    quantity=item["quantity"],
+                    price=item["price"],
+                    user=request.user,
+                )
+
+        else:
+            order = Order.objects.create(
+                full_name=name,
+                email=email,
+                shipping_address=shipping_address,
+                amount_paid=total_cost,
+            )
+
+            # order_id = order.pk
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    quantity=item["quantity"],
+                    price=item["price"],
+                )
+        order_success = True
+        return JsonResponse({"success": order_success})
+
+
 def payment_success(request):
+    # clear shopping cart
+
+    for key in list(request.session.keys()):
+        if key == "session_key":
+            del request.session[key]
+
     return render(request, "payment/payment_success.html")
 
 
